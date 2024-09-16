deeb1c345ae94e2d65b5467c35db8f7f95556d51
Facundo Gayoso <facusbrr@gmail.com>
2024-09-16 20:23:20 -0300

    Corrigiendo el codigo

--- backend/src/routes/todos.routes.js
+++ backend/src/routes/todos.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createTodo,
+  deleteTodo,
   getAllTodosCtrl,
 } from "../controllers/todos.controllers.js";
 import validarJwt from "../middlewares/validar-jwt.js";
@@ -9,4 +10,5 @@ const todosRouter = Router();
 
 todosRouter.get("/", validarJwt, getAllTodosCtrl);
 todosRouter.post("/", validarJwt, createTodo);
+todosRouter.delete("/:id", validarJwt, deleteTodo);
 export { todosRouter };
